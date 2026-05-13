39ef0451fb365555e7575a7a272320d632163a86
Luis <luisleonrios2023@gmail.com>
2026-05-13 11:38:27 +0200

    se añade fichero js para el header

---
+++ js/header.js
@@ -0,0 +1,22 @@
+// Selecciona elementos del header
+const icono_menu = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".navegacion");
+
+// Comprueba que existen antes de usar
+if (icono_menu && menu) {
+
+
+    icono_menu.addEventListener("click", () => {
+        console.log(icono_menu);
+
+        // Activa/desactiva menú
+        menu.classList.toggle("activa");
+
+        // Accesibilidad
+        const abierto = menu.classList.contains("activa");
+
+        icono_menu.setAttribute("aria-expanded", abierto);
+
+    });
+
+}
